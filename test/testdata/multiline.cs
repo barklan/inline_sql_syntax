f7eb18be504d02e5ca7410b61559ad9e376fa0f2
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-05-31 12:49:30 +0300

    refactor(ext): separate C# syntax

---
+++ test/testdata/multiline.cs
@@ -0,0 +1,11 @@
+string query = @"--sql
+    select * FROM book WHERE id = 34;";
+
+string another = @"--sql
+	select * from book
+    where id = 34;";
+
+string more = @"SELECT *
+from book;";
+
+string queryx = "SELECT * from book;"
